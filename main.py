cfad853ebbbd2ed3deb6af45752479cbb8b1129d
cookie-shruti <69342765+cookie-shruti@users.noreply.github.com>
2022-07-18 00:42:46 +0530

    main files

---
+++ main.py
@@ -0,0 +1,13 @@
+import time;
+from plyer import notification
+
+
+if __name__ =="__main__":
+    notification.notify(
+    title = "Please Drink Water",
+    message= "Reminder to Drink water time and Again,It is good for health.",
+    app_icon = "D:\College\PROJECTS\Python Scripts\Drink.ico",
+    timeout=10
+  )
+
+
